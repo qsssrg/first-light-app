6e2871e542bc63cb3e75111f74764f05df07278f
Rintaro Sonoda <sonoda.visia@gmail.com>
2026-05-06 19:15:00 +0900

    Deploy FIRST LIGHT to GitHub Pages

---
+++ _next/static/qk5OINSPeSUbDlF42ffns/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/first-light-app//_next/:path+",
+        "destination": "/first-light-app/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
