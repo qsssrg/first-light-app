fb7f003b6db3752ac937813623db446e89444696
sonodarintaro <sonoda.visia@gmail.com>
2026-05-08 10:42:25 +0900

    Updates

--- _next/static/bTPMSxPxJ0KmsVR6kiX0y/_buildManifest.js
+++ _next/static/bTPMSxPxJ0KmsVR6kiX0y/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/first-light-app//_next/:path+",
-        "destination": "/first-light-app/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
